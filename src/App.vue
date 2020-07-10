6ddfa43b758a384fe544491af5014b3559eaacef
Ricardo Uribe <ricardoauribe@gmail.com>
2020-07-10 12:41:42 -0500

    Initial files

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyComponent/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyComponent
   }
 }
 </script>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<h1> {{book.name.toUpperCase()}} </h1>
+		<h2> {{book.author}} </h2>
+		<div v> </div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'MyComponent',
+	data(){
+		return {
+			book: {
+				name: "Learning",
+				author: "Richard Uribe"
+			}
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
