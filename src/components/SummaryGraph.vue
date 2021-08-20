3a26e391663304e788f0b05fe6d8c1ee4e7fec82
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-08-19 20:11:04 -0500

    Create svg with dimensions

--- src/components/SummaryGraph.vue
+++ src/components/SummaryGraph.vue
@@ -1,12 +1,22 @@
 <template>
-  
+  <div>
+    <svg class="lineChart" :width="w" :height="h" v-if="ready">
+    </svg>
+  </div>
 </template>
 
 <script>
   import * as d3 from "d3";
-  
+
   export default {
-    name: 'SummaryGraph'
+    name: 'SummaryGraph',
+    data() {
+      return {
+        data: [],
+        h: 400,
+        w: 560,
+      }
+    }
   }
 </script>
 
